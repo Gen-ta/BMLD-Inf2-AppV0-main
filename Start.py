29a0f90ae9bb89982fb7f00e8db79e670d23d660
ayl-in <agoayl01@students.zhaw.ch>
2025-02-27 11:19:43 +0100

    änderung

--- Start.py
+++ Start.py
@@ -6,7 +6,8 @@ st.title("Meine erste Streamlit App")
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
 # Streamlit über den Text unten direkt in die App - cool!
-"""
+File "/mount/src/bmld-inf2-appv0-main/Start.py"
+SyntaxError: unterminated triple-quoted string literal (detected at line 16)
 Diese App wurde von folgenden Personen entwickelt:
 - Aylin Ago (agoayl01@students.zhaw.ch)
 - Genta Arifi (arifigen@students.zhaw.ch)
